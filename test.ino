dd7b4d6395e4510ebc11d3cdcd14b13453cd29e9
JensWagner <ing.j.wagner@gmx.de>
2025-11-14 17:59:07 +0100

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,26 +1,17 @@
-#include "Dezibot.h"
-
+#include <Dezibot.h>
 
 Dezibot dezibot = Dezibot();
 void setup() {
-  // put your setup code here, to run once:
-dezibot.begin();
+    dezibot.begin();   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- for (int d = 0; d < 255; d++) {
-      dezibot.multiColorLight.setLed(ALL,dezibot.multiColorLight.color(d,0,255-d));
-      delay(2);
-  }
-
-  for (int d = 0; d < 255; d++) {
-      dezibot.multiColorLight.setLed(ALL, dezibot.multiColorLight.color(255-d, d, 0));
-      delay(2);
-   }
-
-  for (int d = 0; d < 255; d++) {
-      dezibot.multiColorLight.setLed(ALL, dezibot.multiColorLight.color(0, 255-d, d));
-      delay(2);
-  }
+dezibot.motion.move();
+delay(1000);
+dezibot.motion.rotateAntiClockwise();
+delay(1000);
+dezibot.motion.rotateClockwise();
+delay(1000);
+dezibot.motion.stop();
+delay(1000);
 }
